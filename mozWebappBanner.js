b7f87b0fb816ad296c617f9092efcee1faf5689c
Paddy O'Reilly <github@smallroomstudios.net>
2013-05-07 01:04:48 +0100

    Working release. TODO: Translations

--- mozWebappBanner.js
+++ mozWebappBanner.js
@@ -10,16 +10,23 @@
  */
 
 var mozBanr = {
-  init: function(manifestURL){
-    var _this;
+  init: function(manifest){
+    var _this, path, manifest;
 
-    if(!window.localStorage[this.name]){ //navigator.mozApps
+    if(navigator.mozApps && !window.localStorage[this.name]){ //
       _this = this;
 
-      manifestURL = manifestURL || "/manifest.webapp";
-      _this.util.get(manifestURL, function(json){
-        var manifest, searchURL;
+      // Get path for manifest
+      path = window.location.pathname.split('/');
+      path[path.length-1] = '';
 
+      manifest =  manifest || "/manifest.webapp"; // Set default if absent
+      manifest = manifest.substr(0,1) == "/" ? manifest : path.join('/') + manifest;
+
+      _this.manifestURL = window.location.protocol +"//"+ window.location.hostname + manifest;
+
+      _this.util.get(_this.manifestURL, function(json){
+        var manifest, searchURL;
         _this.manifest = JSON.parse(json);
 
         searchURL = _this.endpoints.search.replace('<%app_name%>', _this.manifest.name);
@@ -50,19 +57,26 @@ var mozBanr = {
     app.name = this.app.name || this.manifest.name;
     app.description = this.app.description || this.manifest.description;
     app.icon = this.manifest.icons['128'] || this.manifest.icons['64'] || this.manifest.icons['48'] || this.manifest.icons['32'] || this.manifest.icons['24'] || this.manifest.icons['16'];
+    app.install = this.strings[this.locale]['install'];
 
     header = document.createElement('header');
     header.id = this.name;
-    header.innerHTML = '<img id="mozBanr-logo" src="'+app.icon+'"/><article id="mozBanr-copy"><h1 id="mozBanr-name">' + app.name + '</h1><h2 id="mozBanr-description">' + app.description + '</h2></article><div id="mozBanr-install">Install</div><div id="mozBanr-close">X</div><div id="mozBanr-clear"></div>';
+    header.innerHTML = '<nav><img id="mozBanr-close" src="'+app.icon+'"/><div class="divider"></div></nav><img id="mozBanr-logo" src="http://dublinbikes2go.com/apple-touch-icon.png"/><article id="mozBanr-copy"><h1 id="mozBanr-name">'+app.name+'</h1><h2 id="mozBanr-description">'+app.description+'</h2></article><button id="mozBanr-install">'+app.install+'</button><div class="divider divder-right"></div><div id="mozBanr-clear"></div>';
 
-    // <img id="mozBanr-logo" src="http://dublinbikes2go.com/apple-touch-icon.png"/>
-    // <article id="mozBanr-copy">
-    //   <h1 id="mozBanr-name">Dublin Bikes 2 Go</h1>
-    //   <h2 id="mozBanr-description">Infomration about Dublin Bikes for all mobile phones</h2>
-    // </article>
-    // <div id="mozBanr-install">test</div>
-    // <div id="mozBanr-close">X</div>
-    // <div id="mozBanr-clear"></div>
+    // <header id="mozBanr">
+    //   <nav>
+    //     <img id="mozBanr-close" src="https://marketplace.cdn.mozilla.net/media/img/mkt/icons/close.png"/>
+    //     <div class="divider"></div>
+    //   </nav>
+    //   <img id="mozBanr-logo" src="'+app.icon+'"/>
+    //   <article id="mozBanr-copy">
+    //     <h1 id="mozBanr-name">'+app.name+'</h1>
+    //     <h2 id="mozBanr-description">'+app.description+'</h2>
+    //   </article>
+    //   <button id="mozBanr-install">'+app.install+'</button>
+    //   <div class="divider divder-right"></div>
+    //   <div id="mozBanr-clear"></div>
+    // </header>
 
     document.body.insertBefore(header, document.body.firstChild);
   },
@@ -70,8 +84,19 @@ var mozBanr = {
     var _this = this;
 
     document.getElementById('mozBanr-close').onclick = function(){
-      this.parentNode.parentNode.removeChild(this.parentNode);
+      var banner = document.getElementById(_this.name);
+      banner.parentNode.removeChild(banner);
+
       window.localStorage.setItem(_this.name, _this.name);
+    }
+
+    document.getElementById('mozBanr-install').onclick = function(){alert(_this.manifestURL);
+      var app = navigator.mozApps.install(_this.manifestURL);
+      app.onsuccess = function(data) {
+        // Prevent the banner appearing again
+        window.localStorage.setItem(_this.name, _this.name);
+      };
+
     }
   },
 
@@ -107,6 +132,11 @@ var mozBanr = {
   },
 
   locale: 'en',
+  strings: {
+    en : {
+      install: "Install"
+    }
+  },
 
   app : false,
   manifest : false,
